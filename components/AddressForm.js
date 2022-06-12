87af3cecd92a3e1844c4a76eb69239200a40d283
Eugen Dirksen <eugen@candlesofwisdom.com>
2022-06-12 21:35:27 +0200

    form and calculations working

--- components/AddressForm.js
+++ components/AddressForm.js
@@ -10,15 +10,117 @@ import IconLeft from '../public/icon-left.svg';
 import { BigButton, SmallButton } from './Buttons';
 import { yupResolver } from '@hookform/resolvers/yup';
 import React from 'react';
-import FormValidation from './FormValidation';
 import { GetTotals } from './Calculations';
+import * as yup from 'yup';
 
 export default function AddressForm() {
   const { subTotalPrice } = GetTotals();
   const hydrated = useHydration();
   const router = useRouter();
   const setBuyerData = useStore(state => state.setBuyerData);
-  const schema = FormValidation;
+  const schema = yup
+    .object({
+      DifferentShipping: yup.boolean(),
+      showShippingAddress: yup.boolean(),
+      BillingFirstName: yup
+        .string()
+        .trim()
+        .required('required')
+        .max(20, '${max} characters max'),
+      BillingLastName: yup
+        .string()
+        .trim()
+        .required('required')
+        .max(30, '${max} characters max'),
+      BuyerEmail: yup
+        .string()
+        .trim()
+        .required('required')
+        .email('email incorrect')
+        .max(40, '${max} characters max'),
+      BillingCompany: yup.string().trim().max(30, '${max} characters max'),
+      BillingOptionalLine: yup.string().trim().max(30, '${max} characters max'),
+      BillingStreetAndNumber: yup
+        .string()
+        .trim()
+        .required('required')
+        .max(40, '${max} characters max'),
+      BillingZip: yup
+        .string()
+        .trim()
+        .required('required')
+        .max(12, '${max} characters max'),
+      BillingCity: yup
+        .string()
+        .trim()
+        .required('required')
+        .max(20, '${max} characters max'),
+      BillingCountry: yup
+        .string()
+        .trim()
+        .required('required')
+        .max(20, '${max} characters max'),
+
+      ShippingFirstName: yup
+        .string()
+        .trim()
+        .max(20, '${max} characters max')
+        .when('DifferentShipping', {
+          is: true,
+          then: yup
+            .string()
+            .required('required')
+            .trim()
+            .max(20, '${max} characters max'),
+        }),
+
+      ShippingLastName: yup.string().when('DifferentShipping', {
+        is: true,
+        then: yup
+          .string()
+          .required('required')
+          .trim()
+          .max(20, '${max} characters max'),
+      }),
+
+      ShippingCompany: yup.string().trim().max(30, '${max} characters max'),
+      ShippingOptionalLine: yup.string().max(30, '${max} characters max'),
+      ShippingStreetAndNumber: yup.string().when('DifferentShipping', {
+        is: true,
+        then: yup
+          .string()
+          .required('required')
+          .trim()
+          .max(40, '${max} characters max'),
+      }),
+      ShippingZip: yup.string().when('DifferentShipping', {
+        is: true,
+        then: yup
+          .string()
+          .required('required')
+          .trim()
+          .max(12, '${max} characters max'),
+      }),
+
+      ShippingCity: yup.string().when('DifferentShipping', {
+        is: true,
+        then: yup
+          .string()
+          .required('required')
+          .trim()
+          .max(30, '${max} characters max'),
+      }),
+
+      ShippingCountry: yup.string().when('DifferentShipping', {
+        is: true,
+        then: yup
+          .string()
+          .required('required')
+          .trim()
+          .max(30, '${max} characters max'),
+      }),
+    })
+    .required();
 
   const {
     register,
