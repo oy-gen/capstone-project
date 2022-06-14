a4533ccf136be0b2a5a4aa99293a404f1fff672e
Eugen Dirksen <eugen@candlesofwisdom.com>
2022-06-14 09:30:10 +0200

    finish branch

---
+++ pages/shopping.js
@@ -0,0 +1,18 @@
+import useStore from '../hooks/useStore';
+import ProductCard from '../components/ProductCard';
+import IndexNav from '../components/IndexNav';
+import Header from '../components/Header';
+
+export default function Home() {
+  const products = useStore(state => state.products);
+
+  return (
+    <>
+      {products.map(product => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+      <Header />
+      <IndexNav />
+    </>
+  );
+}
